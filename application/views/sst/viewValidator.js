8ee22024f286499a027732e98859f766b240ea66
Hernan Yupanqui <hernan.yupanqui.prieto@gmail.com>
2021-04-26 17:03:01 -0500

    corrigiendo errores en el texto de la confirmacion

--- application/views/sst/viewValidator.js
+++ application/views/sst/viewValidator.js
@@ -3,7 +3,7 @@ Esta funcion valida si es que un documento de SST ha sido visto y confirmado en 
 Llama a las demas funciones en este documento para lograrlo
 */
 
-export function viewValidator(document_name) {
+export function viewValidator(document_name, user) {
     let data = new FormData;
     data.append("document_name", document_name);
 
@@ -14,7 +14,7 @@ export function viewValidator(document_name) {
     .then(response => response.text())
     .then(data => {
         if (data != "1") {  // 1 means that it was confirmed before
-            requestConfirmation(document_name);
+            requestConfirmation(document_name, user);
         }
     });
 
@@ -22,7 +22,7 @@ export function viewValidator(document_name) {
 };
 
 // Esta funcion se encarga de solicitar la confirmacion al cliente y comunicar si fue confirmado o no
-export function requestConfirmation(the_document_name, user = {nombres: "Hernan", puesto: "Administrador", nro_documento: "78945612"}) {
+export function requestConfirmation(the_document_name, user) {
     let current_date = new Date();
     current_date = `${current_date.getDate()}/${current_date.getMonth()}/${current_date.getFullYear()}`;
 
@@ -30,25 +30,27 @@ export function requestConfirmation(the_document_name, user = {nombres: "Hernan"
     let confirmation_message = "";
     let confirmation_title = "";
     if(the_document_name == "Reglamento Interno de SST") {
+
         confirmation_message = `<p>Conste por el presente documento, que, he recibido y se me ha capacitado 
             sobre el Reglamento Interno de Seguridad y Salud en el Trabajo de INNOMEDIC INTERNATIONAL 
             E.I.R.L. El cual me comprometo a cumplir estrictamente en todas sus normas y dispositivos, 
             para lo cual firmo el presente cargo.</p>
         `;
-
         confirmation_title = "CARGO DE RECEPCIÓN DE RISST";
+
     } else {
+
         confirmation_message = `<p>Al dar click en firmar aceptas que has recibido este documento, 
             el cual quedara registrado en nuestra base de datos</p>
-        `;
-        
-        confirmation_title = "Confirmacion de Lectura"
+        `;        
+        confirmation_title = `Confirmacion de Lectura "${the_document_name}"`;
+
     }
      
 
     Swal.fire({
         title: `${confirmation_title}`,
-        html: `<p>Estas a punto de leer "${the_document_name}".</p>
+        html: `
         ${confirmation_message}
         <div><strong>Nombre: </strong>
             <span>${!user.nombres ? "": user.nombres} 
